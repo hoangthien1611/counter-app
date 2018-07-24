2491e5908dc05e944fc6c44e2700691798b940cb
Thien Nguyen <hoangthien1611@gmail.com>
2018-07-24 17:30:08 +0700

    Finish project

---
+++ src/components/navbar.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+// Use Stateless Funtional Component
+
+const Navbar = props => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        Navbar{" "}
+        <span className="badge badge-pill badge-secondary">
+          {props.totalCounters}
+        </span>
+      </a>
+    </nav>
+  );
+};
+
+export default Navbar;
